5fea23039c1b31f2b40a75d4091faaecf7447b45
lukwlw <luk_w@op.pl>
2017-03-27 21:56:31 +0200

    First working version

---
+++ keywords/__main__.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+from keywords.forms.mainform import KeywordsMainForm
+
+
+root = tk.Tk()
+win = KeywordsMainForm(root)
+root.mainloop()
---
+++ keywords/tools/extractor.py
@@ -0,0 +1,45 @@
+import re
+
+from bs4 import BeautifulSoup
+
+
+class ContentExtractor:
+    """Tool to read informations from web page
+
+    Uses BeautifulSoup module to parse web page content"""
+
+    def __init__(self, page):
+        self._content = None
+        self._page = b''
+        if isinstance(page, bytes):
+            self._page = page
+        self.init_parser()
+        # TODO
+        # set page by property ?
+
+    def init_parser(self):
+        self._content = BeautifulSoup(self._page, 'html.parser')
+        # TODO
+        # try/except ?
+        # suppressing wornings from bs ?
+
+    def get_keywords(self):
+        keywords = None
+        if self._content:
+            if self._content.head:
+                # find meta with name = keywords/KEYWORDS/Keywords/etc.
+                meta = self._content.head.find_all('meta',
+                                                   attrs={'name': re.compile('^keywords', re.I),
+                                                          'content': True})
+                if len(meta) > 0:
+                    meta = meta[0]  # first occurrence of keywords meta - should by only one
+                    keywords = meta['content']
+
+        return keywords
+
+    def get_text(self):
+        body_text = None
+        if self._content:
+            if self._content.body:
+                body_text = self._content.body.get_text()
+        return body_text
