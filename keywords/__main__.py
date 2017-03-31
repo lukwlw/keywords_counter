4d4ca25760ce30a6fbe73dd98f7b7421492ef81f
lukwlw <luk_w@op.pl>
2017-03-31 21:24:23 +0200

    Comments

--- keywords/__main__.py
+++ keywords/__main__.py
@@ -10,4 +10,3 @@ root.mainloop()
 # Future works
 # Add more checks in loader to test URLs. Consider adding http if not exists in url
 # Add logging - logging.xxxxx(), warnings.warn(), etc. to make app more verbose
-# Add additional button in main form to clear the current results
